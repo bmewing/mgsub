a48138670b688fdfe6a853ffe109b902e1366e56
Mark Ewing <mewing@eastman.com>
2018-03-13 09:34:05 -0400

    new algorithmic method
    using gregexpr now

--- R/mgsub.R
+++ R/mgsub.R
@@ -8,7 +8,6 @@
 #' @param replacement Character string equal in length to pattern or of length one which are 
 #' a replacement for matched pattern.
 #' @param recycle logical. should replacement be recycled if lengths differ?
-#' @param conversions DEPRECATED - will be removed in a later release
 #' @param \dots arguments to pass to \code{\link[base]{regexpr}} / \code{\link[base]{sub}}
 #' @rdname mgsub
 #' @return Converted string.
@@ -20,83 +19,78 @@
 #'       ignore.case=TRUE)
 #' @export
 
-mgsub = function(string,pattern,replacement,recycle=FALSE,conversions=list(),...){
+mgsub = function(string,pattern,replacement,recycle=FALSE,...){
   sna = !is.na(string)
-  if((missing(replacement) & !missing(pattern)) | (missing(replacement) & missing(pattern) & length(conversions) > 0)){
-    warning("Calling mgsub with a named list is deprecated. You may continue to call mgsub_dict.")
-    passthrough = if(missing(pattern)) conversions else pattern
-    result = mgsub_dict(string[sna],passthrough,...)
-  } else {
-    if(!is.logical(recycle)) stop("Recycle must be a boolean")
-    if(!recycle & length(pattern) != length(replacement)) stop("pattern and replacement vectors must be the same length")
-    if(length(replacement) > length(pattern)){
-      warning("You provided more replacements than search strings - some will be dropped")
-      replacement = replacement[seq_along(pattern)]
-    }
-    if(recycle & length(pattern) != length(replacement)){
-      replacement = rep(replacement,ceiling(length(pattern) / length(replacement)))[seq_along(pattern)]
-    } 
-    names(replacement) = pattern
-    result = unlist(worker(string[sna], pattern, replacement,...))
+  if(!is.logical(recycle)) stop("Recycle must be a boolean")
+  if(!recycle & length(pattern) != length(replacement)) stop("pattern and replacement vectors must be the same length")
+  if(length(replacement) > length(pattern)){
+    warning("You provided more replacements than search strings - some will be dropped")
+    replacement = replacement[seq_along(pattern)]
   }
+  if(recycle & length(pattern) != length(replacement)){
+    replacement = rep(replacement,ceiling(length(pattern) / length(replacement)))[seq_along(pattern)]
+  } 
+  #names(replacement) = pattern
+  result = vapply(string[sna],worker,c(""),USE.NAMES = FALSE,pattern=pattern,replacement=replacement,...)
   string[sna] = result
   return(string)
 }
 
-#' Safe, multiple gsub
-#' 
-#' \code{mgsub_dict} - Dictionary version of \code{mgsub} that 
-#' takes a vector of replacement strings, named with their matching search terms
-#' and applies them to a single string to be modified.
-#' 
-#' @rdname mgsub
-#' @export
-
-mgsub_dict = function(string,conversions=list(),...){
-  if(is.null(names(conversions))) stop("The object provided for `conversions` must be named")
-  return(unlist(worker(string,names(conversions),unlist(conversions),...)))
-}
-
-worker = function(string,p,r,...){
-  if(length(string)==1){
-    return(w2(string,p,r,...))
-  }
-  if(any(grepl("\\$|\\^",p))){
-    return(lapply(string,w2,p=p,r=r,...))
-  }
-  div = c("\001", "\002", "\003", "\004", "\005", "\006", "\a", "\b", 
-                 "\t", "\n", "\v", "\f", "\r", "\016", "\017", "\020", "\021", 
-                 "\022", "\023", "\024", "\025", "\026", "\027", "\030", "\031", 
-                 "\032", "\033", "\034", "\035", "\036", "\037", "\177")
-  while(any(grepl(div[1],string))){
-    div = div[-1]
-  }
-  s = paste(string,collapse = div[1])
-  p = gsub("([^\\])\\.",paste0("\\1[^",div[1],"]"),p)
-  return(strsplit(w2(s,p,r,...),div,fixed=T))
-}
-
-w2 = function(s,p,r,...){
-  newString = ""
-  while(nchar(s) > 0){
-    matches = lapply(p,regexpr,text=s,...)
-    m = unlist(lapply(matches,`[[`,1))
-    if(all(m < 0)){
-      newString = paste0(newString,s)
-      s = ''
+worker = function(string,pattern,replacement,...){
+  x = do.call(rbind,lapply(seq_along(pattern),getMatches,string=string,pattern=pattern,...))
+  if(nrow(x) > 1){
+    x = x[order(x[,3],decreasing = T),]
+    x = filterOverlap(x)
+    if(!is.null(dim(x))){
+      x = x[order(x[,2]),]
     } else {
-      m[m<0] = Inf
-      fr = which(m==min(m))
-      nc = unlist(lapply(matches,attr,"match.length"))[fr]
-      fr = fr[which.max(nc)]
-      nc = nc[which.max(nc)]
-      fp = unlist(lapply(matches,`[[`,1))[fr]
-      if(fp > 1){
-        newString = paste0(newString,substr(s,1,fp-1))
-      }
-      newString = paste0(newString,sub(p[fr],r[fr],substr(s,fp,fp+nc-1),...))
-      s = substr(s,fp+nc,nchar(s))
+      return(singleReplace(x,pattern,string,replacement,...))
     }
   }
-  return(newString)
+  for(i in nrow(x):1){
+    s = x[i,2]
+    e = x[i,4]
+    p = pattern[x[i,1]]
+    pre = if(s > 1) substr(string,1,s-1) else ""
+    r = sub(p,replacement[pattern == p],substr(string,s,e),...)
+    end = if(e < nchar(string)) substr(string,e+1,nchar(string)) else ""
+    string = paste0(pre,r,end)
+  }
+  return(string)
+}
+
+singleReplace = function(x,pattern,string,replacement,...){
+  s = x[2]
+  e = x[4]
+  p = pattern[x[1]]
+  pre = if(s > 1) substr(string,1,s-1) else ""
+  r = sub(p,replacement[pattern == p],substr(string,s,e),...)
+  end = if(e < nchar(string)) substr(string,e+1,nchar(string)) else ""
+  string = paste0(pre,r,end)
+  return(string)
+}
+
+getMatches = function(string,pattern,i,...){
+  tmp = gregexpr(pattern[i],string,...)
+  start = tmp[[1]]
+  length = attr(tmp[[1]],"match.length")
+  return(matrix(cbind(i,start,length,start+length-1),ncol=4))
+}
+
+filterOverlap = function(x){
+  for(i in nrow(x):2){
+    s = x[i,2]
+    ps = x[1:(i-1),2]
+    e = x[i,4]
+    pe = x[1:(i-1),4]
+    if(any(ps <= s & pe >= s)){
+      x = x[-i,]
+      next
+    }
+    if(any(ps <= e & pe >= e)){
+      x = x[-i,]
+      next
+    }
+  }
+  return(x)
 }
--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 ***A safe, multiple, simultaneous string substitution function***
 
-[![Build Status](https://travis-ci.org/bmewing/mgsub.svg?branch=dev)](https://travis-ci.org/bmewing/mgsub) [![Coverage Status](https://img.shields.io/codecov/c/github/bmewing/mgsub/dev.svg)](https://codecov.io/github/bmewing/mgsub?branch=dev) [![CRAN\_Status\_Badge](http://www.r-pkg.org/badges/version/mgsub)](https://CRAN.R-project.org/package=mgsub) ![](http://cranlogs.r-pkg.org/badges/mgsub)
-
 ## Why do I want this?
 You have a string you want to make substitutions on. You want to make many different substitutions at the same time and you want them done in a safe way.
 For example, you want to shift each word in "hey, how are you?" to the left by replacing "hey" with "how", "how" with "are, etc.  Existing functions either do not support vectorization (e.g. `gsub`), don't support simultaneous substition (e.g. `stringr::str_replace_all`) or do the replacements in an unsafe manner (e.g. `qdap::mgsub`).
@@ -12,34 +10,14 @@ This is a lightweight, pure R function with no dependencies to avoid package blo
 ## Install it!
 
 ```r   
-#install from this github repo
-devtools::install_github("bmewing/mgsub")
+install.packages('mgsub')
+#Install the latest version from GitHub
+#devtools::install_github("bmewing/mgsub")
 ```
 
 ## Usage
 
-There are currently two supported method for specifying the patterns to match and their associated replacements. `r mgsub::mgsub` is a NSE wrapper that determines the method supplied and calls either `r mgsub::mgsub_dict` for dictionary style specification or `r mgusb::mgsub_vm` for a vector method specification.  You do not need to name the arguments when supplying them.
-
-### Dictionary method
-
-```r
-mgsub::mgsub(string,conversions=list(),...)
-```
-
-Conversions are supplied using a named list.  The name is the matching pattern, the value is the replacement.
-
-```r
-mgsub::mgsub("hey, how are you?",list("hey"="how","how"="are","are"="you","you"="hey"))
-```
-
-Matches and replacements can be supplied as regex expressions.  Additional arguments can be passed to the `sub`/`gsub`/`gregexpr` family of internal functions.
-
-```r
-mgsub::mgsub("Dopazamine is not the same as Dopachloride and is still fake.", 
-             list("[Dd]opa(.*?mine)"="Meta\\1","fake"="real"),ignore.case=F)
-```
-
-### Vector method
+Simply pass in a vector of strings to be modified, a vector of patterns to match and a vector of replacements. Then watch as they are safely, simultaneously replaced!
 
 ```r
 mgsub::mgsub(string,pattern=c(),replacement=c(),recycle=FALSE,...)
